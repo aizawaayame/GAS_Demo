7d30009d20c7fc2899d07af99c76830c26bb14d2
tanedanobata <95639838+aizawaayame@users.noreply.github.com>
2024-08-15 17:34:36 +0800

    2.7 Character Blueprint Setup

---
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -0,0 +1,14 @@
+
+
+
+#include "Character/AuraCharacter.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AAuraCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
