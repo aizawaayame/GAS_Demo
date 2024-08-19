c7fd5fabe4a880ef914650457665526ec57e6cac
tanedanobata <95639838+aizawaayame@users.noreply.github.com>
2024-08-19 22:56:11 +0800

    2.12 Game Mode

--- Source/Aura/Private/AuraPlayer.cpp
+++ Source/Aura/Private/AuraPlayer.cpp
@@ -3,3 +3,16 @@
 
 #include "AuraPlayer.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAuraPlayer::AAuraPlayer()
+{
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0, 400, 0);
+	GetCharacterMovement()->bConstrainToPlane = true;
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
